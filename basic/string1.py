ec8fae823b4ae743189bc0c0d2bbfc941097d826
jeromecox <jeromecox@hotmail.com>
2022-12-05 15:55:00 -0600

    string1 solved

--- basic/string1.py
+++ basic/string1.py
@@ -24,8 +24,10 @@
 # So donuts(5) returns 'Number of donuts: 5'
 # and donuts(23) returns 'Number of donuts: many'
 def donuts(count):
-  # +++your code here+++
-  return
+  if count < 10:
+    return "Number of donuts: " + str(count)
+  else:
+    return "Number of donuts: many"
 
 
 # B. both_ends
@@ -33,23 +35,38 @@ def donuts(count):
 # and the last 2 chars of the original string,
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
-def both_ends(s):
-  # +++your code here+++
-  return
+def both_ends(our_string):
+  if len(our_string) < 2:
+    return ""
+  else:
+    return our_string[:2] + our_string[-2:]
+  # return
 
 
 # C. fix_start
 # Given a string s, return a string
-# where all occurences of its first char have
+# where all occurrences of its first char have
 # been changed to '*', except do not change
 # the first char itself.
 # e.g. 'babble' yields 'ba**le'
 # Assume that the string is length 1 or more.
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
-def fix_start(s):
-  # +++your code here+++
-  return
+# def fix_start(my_string):
+#   string_first = my_string[0]
+#   new_string = string_first
+#   for letter in my_string[1:]:
+#     if letter == string_first:
+#       new_string += '*'
+#     else:
+#       new_string += letter
+#   return new_string
+
+
+def fix_start(my_string):
+  new_string = my_string[1:]
+  answer = my_string[0] + new_string.replace(my_string[0], "*")
+  return answer
 
 
 # D. MixUp
@@ -60,8 +77,7 @@ def fix_start(s):
 #   'dog', 'dinner' -> 'dig donner'
 # Assume a and b are length 2 or more.
 def mix_up(a, b):
-  # +++your code here+++
-  return
+  return b[:2] + a[2:] + " " + a[:2] + b[2:]
 
 
 # Provided simple test() function used in main() to print
